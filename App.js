1c544eb1972613dcbf99943dceecdc6841629c68
Archaeologist03 <omniiknight@hotmail.com>
2020-03-02 22:53:01 +0100

    lection 11 - implementing header buttons

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
