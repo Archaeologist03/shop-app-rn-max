7e9225a0a51c7b947115e022563cb71dfcf0d6e4
Archaeologist03 <omniiknight@hotmail.com>
2020-03-04 22:43:48 +0100

    lection 13 - adding logic to delete items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
 
 const CartScreen = props => {
 	const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -18,8 +19,11 @@ const CartScreen = props => {
 				sum: state.cart.items[key].sum,
 			});
 		}
-		return transformedCartItems;
+		return transformedCartItems.sort((a, b) =>
+			a.productId > b.productId ? 1 : -1,
+		);
 	});
+	const dispatch = useDispatch();
 
 	return (
 		<View style={styles.screen}>
@@ -34,20 +38,20 @@ const CartScreen = props => {
 					disabled={cartItems.length === 0}
 				/>
 			</View>
-			<View>
-				<FlatList
-					data={cartItems}
-					keyExtractor={item => item.productId}
-					renderItem={itemData => (
-						<CartItem
-							quantity={itemData.item.quantity}
-							title={itemData.item.productTitle}
-							amount={itemData.item.sum}
-							onRemove={() => {}}
-						/>
-					)}
-				/>
-			</View>
+			<FlatList
+				data={cartItems}
+				keyExtractor={item => item.productId}
+				renderItem={itemData => (
+					<CartItem
+						quantity={itemData.item.quantity}
+						title={itemData.item.productTitle}
+						amount={itemData.item.sum}
+						onRemove={() => {
+							dispatch(cartActions.removeFromCart(itemData.item.productId));
+						}}
+					/>
+				)}
+			/>
 		</View>
 	);
 };
